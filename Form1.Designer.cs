8290da54b9b5c8385f239cfcb8cf40b38c71a147
Saumyabrata Bhattacharya <saumyabrata@gmail.com>
2024-06-02 12:04:38 +0530

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace Excel2Mysql
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.dataGridView1 = new System.Windows.Forms.DataGridView();
+			this.button1 = new System.Windows.Forms.Button();
+			this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+			this.panel1 = new System.Windows.Forms.Panel();
+			this.panel2 = new System.Windows.Forms.Panel();
+			this.panel3 = new System.Windows.Forms.Panel();
+			this.panel4 = new System.Windows.Forms.Panel();
+			this.panel5 = new System.Windows.Forms.Panel();
+			this.panel6 = new System.Windows.Forms.Panel();
+			((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+			this.panel1.SuspendLayout();
+			this.panel5.SuspendLayout();
+			this.panel6.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// dataGridView1
+			// 
+			this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+			this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.dataGridView1.Location = new System.Drawing.Point(0, 0);
+			this.dataGridView1.Name = "dataGridView1";
+			this.dataGridView1.RowHeadersWidth = 51;
+			this.dataGridView1.RowTemplate.Height = 24;
+			this.dataGridView1.Size = new System.Drawing.Size(639, 348);
+			this.dataGridView1.TabIndex = 0;
+			// 
+			// button1
+			// 
+			this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.button1.Location = new System.Drawing.Point(266, 3);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(111, 46);
+			this.button1.TabIndex = 1;
+			this.button1.Text = "Import";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// openFileDialog1
+			// 
+			this.openFileDialog1.FileName = "openFileDialog1";
+			// 
+			// panel1
+			// 
+			this.panel1.Controls.Add(this.panel6);
+			this.panel1.Controls.Add(this.panel5);
+			this.panel1.Controls.Add(this.panel4);
+			this.panel1.Controls.Add(this.panel3);
+			this.panel1.Controls.Add(this.panel2);
+			this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.panel1.Location = new System.Drawing.Point(0, 0);
+			this.panel1.Name = "panel1";
+			this.panel1.Size = new System.Drawing.Size(800, 450);
+			this.panel1.TabIndex = 2;
+			// 
+			// panel2
+			// 
+			this.panel2.Dock = System.Windows.Forms.DockStyle.Left;
+			this.panel2.Location = new System.Drawing.Point(0, 0);
+			this.panel2.Name = "panel2";
+			this.panel2.Size = new System.Drawing.Size(140, 450);
+			this.panel2.TabIndex = 2;
+			// 
+			// panel3
+			// 
+			this.panel3.Dock = System.Windows.Forms.DockStyle.Top;
+			this.panel3.Location = new System.Drawing.Point(140, 0);
+			this.panel3.Name = "panel3";
+			this.panel3.Size = new System.Drawing.Size(660, 50);
+			this.panel3.TabIndex = 3;
+			// 
+			// panel4
+			// 
+			this.panel4.Dock = System.Windows.Forms.DockStyle.Right;
+			this.panel4.Location = new System.Drawing.Point(779, 50);
+			this.panel4.Name = "panel4";
+			this.panel4.Size = new System.Drawing.Size(21, 400);
+			this.panel4.TabIndex = 4;
+			// 
+			// panel5
+			// 
+			this.panel5.Controls.Add(this.button1);
+			this.panel5.Dock = System.Windows.Forms.DockStyle.Bottom;
+			this.panel5.Location = new System.Drawing.Point(140, 398);
+			this.panel5.Name = "panel5";
+			this.panel5.Size = new System.Drawing.Size(639, 52);
+			this.panel5.TabIndex = 5;
+			// 
+			// panel6
+			// 
+			this.panel6.Controls.Add(this.dataGridView1);
+			this.panel6.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.panel6.Location = new System.Drawing.Point(140, 50);
+			this.panel6.Name = "panel6";
+			this.panel6.Size = new System.Drawing.Size(639, 348);
+			this.panel6.TabIndex = 6;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Controls.Add(this.panel1);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+			this.panel1.ResumeLayout(false);
+			this.panel5.ResumeLayout(false);
+			this.panel6.ResumeLayout(false);
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.DataGridView dataGridView1;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.OpenFileDialog openFileDialog1;
+		private System.Windows.Forms.Panel panel1;
+		private System.Windows.Forms.Panel panel6;
+		private System.Windows.Forms.Panel panel5;
+		private System.Windows.Forms.Panel panel4;
+		private System.Windows.Forms.Panel panel3;
+		private System.Windows.Forms.Panel panel2;
+	}
+}
+
---
+++ Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.OleDb;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.VisualStyles;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Window;
+
+namespace Excel2Mysql
+{
+	public partial class Form1 : Form
+	{
+		private OleDbConnection c;
+
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+			
+
+		}
+
+		private static OleDbConnection GetConnection(string filename, bool openIt)
+		{
+			//if your data has no header row, change HDR = NO
+			var c = new OleDbConnection($"Provider=Microsoft.ACE.OLEDB.12.0;Data Source='{filename}';Extended Properties=\"Excel 12.0;HDR=YES;IMEX=1\" ");
+			if (openIt)
+				c.Open();
+			return c;
+		}
+
+		private static DataSet GetExcelFileAsDataSet(OleDbConnection conn)
+		{
+			var sheets = conn.GetOleDbSchemaTable(OleDbSchemaGuid.Tables, new[] { default, default, default, "TABLE" });
+			var ds = new DataSet();
+			foreach (DataRow r in sheets.Rows)
+				ds.Tables.Add(GetExcelSheetAsDataTable(conn, r["TABLE_NAME"].ToString()));
+			return ds;
+		}
+
+		private static DataTable GetExcelSheetAsDataTable(OleDbConnection conn, string sheetName)
+		{
+			using (var da = new OleDbDataAdapter($"select * from [{sheetName}]", conn))
+			{
+				var dt = new DataTable() { TableName = sheetName.TrimEnd('$') };
+				da.Fill(dt);
+				return dt;
+			}
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			OpenFileDialog openfile = new OpenFileDialog();
+			openfile.DefaultExt = ".xlsx";
+			openfile.Filter = "Excel files|*.xls;*.xlsx";
+			DialogResult result = openfile.ShowDialog();
+			if (result == DialogResult.OK)
+			{
+				//All sheets
+				DataSet ds;
+				string fname=openfile.FileName;
+				using (c = GetConnection(fname, true))
+					ds = GetExcelFileAsDataSet(c);
+
+				//only one sheet
+				DataTable dt;
+				using (c = GetConnection(fname, true))
+					dt = GetExcelSheetAsDataTable(c, "hospital_registration$");
+				dataGridView1.DataSource = dt;
+			}
+			else if (result == DialogResult.Cancel)
+			{
+				MessageBox.Show("Aborted!!");
+			}
+			
+		}
+
+	}
+}
